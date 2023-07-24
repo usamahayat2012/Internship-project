967e95ef39fbfb4631cf85405a641934b6a7c054
Osama <osamahayat2012@gmail.com>
2023-07-24 04:29:52 -0700

    third commit

--- src/components/BackEnd.vue
+++
@@ -1,9 +0,0 @@
-<template>
-<h4>Back End</h4>
-</template>
-
-<script>
-export default{
-    name:'BackEnd',
-}
-</script>
--- src/components/FrontEnd.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <h4>Front End</h4>
-    </template>
-    
-    <script>
-    export default{
-        name:'FrontEnd',
-    }
-    </script>
